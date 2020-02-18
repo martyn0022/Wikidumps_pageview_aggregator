9fc4e166785ded4cdcb75046d7fc8bb47d229294
martyn lee <32283437+martyn0022@users.noreply.github.com>
2020-02-18 17:20:37 +0100

    changed shebang

--- GenerateAggregatedQviews.sh
+++ GenerateAggregatedQviews.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 mkfifo Alist
 mkfifo Blist
 
-curl https://dumps.wikimedia.org/other/pagecounts-ez/merged/pagecounts-2019-12-views-ge-5-totals.bz2 | bunzip2 | python ProcessPageviewcount.py| head -n 100000 >> Alist & curl https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.bz2 | bunzip2 | python ProcessWikidump.py| head -n 100000 >> Blist & sort Alist Blist | python GenerateRawQviewsApp.py  | sort -k 1 | python GetAggregatedQviews.py > AggregatedQviews.txt
+curl https://dumps.wikimedia.org/other/pagecounts-ez/merged/pagecounts-2019-12-views-ge-5-totals.bz2 | bunzip2 | python ProcessPageviewcount.py >> Alist & curl https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.bz2 | bunzip2 | python ProcessWikidump.py >> Blist & sort Alist Blist | python GenerateRawQviewsApp.py  | sort -k 1 | python GetAggregatedQviews.py > AggregatedQviews.txt
